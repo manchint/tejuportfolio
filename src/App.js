8437f4b38d6ddeb56b4a5cd781248fd9d611ea7b
manchint <152920263+manchite@users.noreply.github.com>
2024-01-21 17:52:04 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './pages/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <About />
+      <Projects />
+      <Contact /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      {/* Your contact form or details go here */}
+    </section>
+  );
+};
+
+export default Contact;
